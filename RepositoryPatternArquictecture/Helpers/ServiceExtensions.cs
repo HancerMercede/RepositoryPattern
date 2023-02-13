8e728ba9bff260077a6e86c4582f67199d035152
Hancer <hancermercedes@gmail.com>
2023-02-13 13:06:31 -0400

    First Commit adding Services Extensions

---
+++ RepositoryPatternArquictecture/Helpers/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+
+namespace RepositoryPatternArquictecture.Helpers;
+
+public static class ServiceExtensions
+{
+    public static void ConfiguredSqlContext(this IServiceCollection services, string connection)
+    {
+        services.AddDbContext<RepositoryContext>(opts =>
+        opts.UseSqlServer(connection));
+    }
+}
